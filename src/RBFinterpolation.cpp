ea9a17998780e27ec649789875c90fc98394c0fc
Nils Malmberg <nilsaido.malmberg@mail.polimi.it>
2024-04-05 13:51:23 +0000

    ADD: predefined rbfunctions

--- src/RBFinterpolation.cpp
+++ src/RBFinterpolation.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include "RBFinterpolation.hpp"
 
 RBFInterpolation::RBFInterpolation(std::function<double(double, double)> rbfunction) : rbfunction(rbfunction) {
@@ -5,17 +6,23 @@ RBFInterpolation::RBFInterpolation(std::function<double(double, double)> rbfunct
 }
 
 double RBFInterpolation::rbMultiquadratic(double r, double r0) {
-    
+    return sqrt(r*r + r0*r0);
 }
 
 double RBFInterpolation::rbInverseMultiquadratic(double r, double r0) {
-
+    return 1.0 / sqrt(r*r + r0*r0);
 }
 
 double RBFInterpolation::rbThinPlateSpline(double r, double r0) {
+    double res{};
 
+    if (r != 0) {
+        res = r*r * log(r / r0);
+    }
+
+    return res;
 }
 
 double RBFInterpolation::rbGaussian(double r, double r0) {
-
+    return exp(-0.5 * r * r / (r0 * r0));
 }
