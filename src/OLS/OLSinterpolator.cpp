cc85b70b6eb89e4d67874016c2b20369c2a94a99
Nils Malmberg <nilsaido.malmberg@mail.polimi.it>
2024-04-14 22:18:15 +0000

    FIX: error in coeff, the first is the constant part

--- src/OLS/OLSinterpolator.cpp
+++ src/OLS/OLSinterpolator.cpp
@@ -9,14 +9,18 @@ Eigen::VectorXd OLSInterpolator::interpolate(const Eigen::MatrixXd& parametersFO
     size_t num_measures{static_cast<size_t>(measurements.size())};
     size_t num_points{static_cast<size_t>(parametersFORinterp.rows())};
     
-    Eigen::VectorXd results = Eigen::VectorXd::Zero(num_points);
+    // Add a column with ones in the first position
+    Eigen::MatrixXd newparameters(parameters.rows(), parameters.cols() + 1);
+    newparameters << Eigen::MatrixXd::Ones(parameters.rows(), 1), parameters;
 
-    Eigen::MatrixXd transpose_parameters{parameters.transpose()};
-    Eigen::VectorXd beta_coeff(num_params);
 
-    Eigen::JacobiSVD<Eigen::MatrixXd> svd(transpose_parameters*parameters, Eigen::ComputeThinU | Eigen::ComputeThinV);
+    Eigen::MatrixXd transpose_parameters{newparameters.transpose()};
+    
+
+    Eigen::JacobiSVD<Eigen::MatrixXd> svd(transpose_parameters*newparameters, Eigen::ComputeThinU | Eigen::ComputeThinV);
     Eigen::MatrixXd inverse_matrix = svd.matrixV() * svd.singularValues().asDiagonal().inverse() * svd.matrixU().transpose();
 
+    Eigen::VectorXd beta_coeff(num_params);
     beta_coeff = inverse_matrix * transpose_parameters * measurements;
 
     // Storage of the weights if the user define the pointer to the VectorXd
@@ -25,10 +29,12 @@ Eigen::VectorXd OLSInterpolator::interpolate(const Eigen::MatrixXd& parametersFO
     }
 
     // Computation of the interpolated value
+    Eigen::VectorXd results{ Eigen::VectorXd::Constant(num_points, beta_coeff(0))} ;
+
     for (size_t i = 0; i < num_points; ++i)
     {
         for (size_t j = 0; j < num_params; ++j) {
-            results(i) += beta_coeff(j) * parametersFORinterp(i,j);
+            results(i) += beta_coeff(j+1) * parametersFORinterp(i,j);
         }
     }
     return results;
